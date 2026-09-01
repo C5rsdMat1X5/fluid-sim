9c2c90e9d7bb62deacdacb34a24ecf839325b599
Matias Henriquez <matias@MacBook-Air-de-Matias.local>
2026-08-31 21:37:49 -0400

    update

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,8 @@
 add_rules("mode.debug", "mode.release")
 
+set_defaultmode("release")
+set_policy("build.ccache", true)
+
 target("fluid-sim")
     add_rules("xcode.application")
     set_languages("c++17")
@@ -8,8 +11,20 @@ target("fluid-sim")
     add_files("src/*.cpp")
     add_files("src/*.mm")
     add_files("src/*.metal")
-
+    
     add_frameworks("Metal")
     add_frameworks("Foundation")
     add_frameworks("QuartzCore")
     add_frameworks("AppKit")
+
+    if is_mode("release") then
+        set_optimize("fastest") 
+        set_symbols("hidden")
+        set_strip("all")
+        add_defines("NDEBUG")
+
+        set_policy("build.optimization.lto", true)
+    elseif is_mode("debug") then
+        set_symbols("debug")
+        set_optimize("none")
+    end
